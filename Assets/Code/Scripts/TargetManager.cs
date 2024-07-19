b8a0b4eac1e234f666ac02cc605e567b4512cd9e
FLY123 <akfakf0509@naver.com>
2024-07-19 16:52:22 +0900

    feat: 게임 진행 뼈대

--- Assets/Code/Scripts/TargetManager.cs
+++ Assets/Code/Scripts/TargetManager.cs
@@ -10,40 +10,49 @@ public class TargetManager : MonoBehaviour
 	[SerializeField] private Vector2 m_middleSpawnPosition = Vector2.zero;
 	[SerializeField] private Vector2 m_leftSpawnPosition = Vector2.left;
 
-	Target m_rightTarget = null;
-	Target m_middleTarget = null;
-	Target m_leftTarget = null;
+	enum Direction { RIGHT, MIDDLE, LEFT };
+	Target m_target;
+	Direction m_targetDirection;
+	GameObject m_targetGameObject;
 
-	private void Start()
+	private void Update()
 	{
-		InputManager.RightKeyDownEvent.AddListener(() => { if (m_rightTarget) Hit(m_rightTarget); });
-		InputManager.UpKeyDownEvent.AddListener(() => { if (m_middleTarget) Hit(m_middleTarget); });
-		InputManager.LeftKeyDownEvent.AddListener(() => { if (m_leftTarget) Hit(m_leftTarget); });
-	}
-	private void Hit(Target target)
-	{
-		m_scoreManager.AddScore(target.TargetData.Score);
-		Destroy(target.gameObject);
+		if (!Input.GetKeyDown(KeyCode.UpArrow) && !Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.LeftArrow)) return;
+
+		Direction typedDirection;
+		if (Input.GetKeyDown(KeyCode.UpArrow)) typedDirection = Direction.MIDDLE;
+		else if (Input.GetKeyDown(KeyCode.RightArrow)) typedDirection = Direction.RIGHT;
+		else typedDirection = Direction.LEFT;
+
+		if (typedDirection == m_targetDirection) Hit();
+		Destroy(m_target.gameObject);
 		Spawn();
+	}
+
+	private void Hit()
+	{
+		m_scoreManager.AddScore(m_target.TargetData.Score);
 	}
 	public void Spawn()
 	{
 		int index = Random.Range(0, m_targetPrefabs.Count);
 		GameObject createdTarget = Instantiate(m_targetPrefabs[index]);
 		int directNumber = Random.Range(0, 3);
+		m_target = createdTarget.GetComponent<Target>();
+		m_targetGameObject = createdTarget;
 		switch (directNumber)
 		{
 			case 0:
-				m_rightTarget = createdTarget.GetComponent<Target>();
-				m_rightTarget.transform.position = m_rightSpawnPosition;
+				m_targetDirection = Direction.RIGHT;
+				m_target.transform.position = m_rightSpawnPosition;
 				break;
 			case 1:
-				m_middleTarget = createdTarget.GetComponent<Target>();
-				m_middleTarget.transform.position = m_middleSpawnPosition;
+				m_targetDirection = Direction.MIDDLE;
+				m_target.transform.position = m_middleSpawnPosition;
 				break;
 			case 2:
-				m_leftTarget = createdTarget.GetComponent<Target>();
-				m_leftTarget.transform.position = m_leftSpawnPosition;
+				m_targetDirection = Direction.LEFT;
+				m_target.transform.position = m_leftSpawnPosition;
 				break;
 		}
 	}
