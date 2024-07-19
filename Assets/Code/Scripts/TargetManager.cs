8e7165d66b526ffe13514e7b752e1ad40d2ca6f0
FLY123 <akfakf0509@naver.com>
2024-07-19 13:58:33 +0900

    feat: scriptableobject and target manager(WIP)

---
+++ Assets/Code/Scripts/TargetManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+	Target rightTarget = null;
+	Target middleTarget = null;
+	Target leftTarget = null;
+}
--- Assets/Scripts/TargetManager.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class TargetManager : MonoBehaviour
-{
-}
