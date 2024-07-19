c07da3acf5315b5426bfd733fe5fabd8eed24e2b
Jinwon <jinwon11022@naver.com>
2024-07-19 16:22:33 +0900

    feat: Title, Main, Option (ing~)

---
+++ Assets/Code/Scripts/TitleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    public bool isStarted = false; //시작여부 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스 클릭 + 터치 감지
+        if (!isStarted && (Input.GetMouseButtonDown(0) || Input.touchCount > 0))
+        {
+            // 게임 시작 상태로 변경
+            isStarted = true;
+            // MainMenu 로드
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
