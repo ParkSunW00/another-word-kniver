c672dde057bce28136643768709d0f64487e8c3a
FLY123 <akfakf0509@naver.com>
2024-07-20 04:07:59 +0900

    feat: 타켓 및 스포트 라이트 스프라이트 적용

--- Assets/Code/Scripts/Target.cs
+++ Assets/Code/Scripts/Target.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-	[SerializeField] private TargetData m_targetData;
+	[SerializeField] TargetData m_targetData;
+	[SerializeField] List<Sprite> m_spiretes = new();
+	[SerializeField] SpriteRenderer m_spriteRenderer;
+	[SerializeField] GameObject m_spotLightGameObject;
+
 	public TargetData TargetData
 	{
 		get { return m_targetData; }
+	}
+
+	private void Start()
+	{
+		int randomIndex = Random.Range(0, m_spiretes.Count);
+		m_spotLightGameObject.SetActive(TargetData.Score > 0);
+		m_spriteRenderer.sprite = m_spiretes[randomIndex];
 	}
 }
