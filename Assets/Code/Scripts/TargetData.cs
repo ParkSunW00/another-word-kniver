a7d9ddf2fe03cf9a7ff74d71dc51de20961aa3fc
FLY123 <akfakf0509@naver.com>
2024-07-19 15:30:41 +0900

    feat: Target scriptable object

---
+++ Assets/Code/Scripts/TargetData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Target Data", menuName = "Scriptable Object/Target Data")]
+public class TargetData : ScriptableObject
+{
+	[SerializeField] private int score = 0;
+	public int Score
+	{
+		get { return score; }
+	}
+}
